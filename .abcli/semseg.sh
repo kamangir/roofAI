85c2f28d6b016e7a61553338cc8f8b6ba0708645
kamangir <arash@kamangir.net>
2023-10-13 19:16:44 -0700

    semseg += train - kamangir/bolt#746

--- .abcli/semseg.sh
+++ .abcli/semseg.sh
@@ -8,9 +8,8 @@ function roofAI_semseg() {
     local task=$(abcli_unpack_keyword $1 help)
 
     if [ $task == "help" ]; then
-        local options="device=cpu|cuda,~download,dryrun,profile=FULL|QUICK|VALIDATION,~upload"
-        abcli_show_usage "semseg predict$ABCUL[$options]$ABCUL<model_object_name>$ABCUL<dataset_object_name>$ABCUL<prediction_object_name>" \
-            "semseg[<model_object_name>].predict(<dataset_object_name>) -> <prediction_object_name>."
+        roofAI_semseg predict "$@"
+        roofAI_semseg train "$@"
 
         [[ "$(abcli_keyword_is $2 verbose)" == true ]] &&
             python3 -m roofAI.semseg --help
@@ -18,13 +17,20 @@ function roofAI_semseg() {
         return
     fi
 
-    if [ "$task" == "predict" ]; then
-        local options=$2
+    local options=$2
 
-        local device=$(abcli_option "$options" device cpu)
-        local do_dryrun=$(abcli_option_int "$options" dryrun 0)
-        local do_download=$(abcli_option_int "$options" download $(abcli_not $du_dryrun))
-        local do_upload=$(abcli_option_int "$options" upload $(abcli_not $du_dryrun))
+    local device=$(abcli_option "$options" device cpu)
+    local do_dryrun=$(abcli_option_int "$options" dryrun 0)
+    local do_download=$(abcli_option_int "$options" download $(abcli_not $du_dryrun))
+    local do_upload=$(abcli_option_int "$options" upload $(abcli_not $du_dryrun))
+
+    if [ "$task" == "predict" ]; then
+        if [ $(abcli_option_int "$options" help 0) == 1 ]; then
+            local options="device=cpu|cuda,~download,dryrun,profile=FULL|QUICK|VALIDATION,~upload"
+            abcli_show_usage "semseg predict$ABCUL[$options]$ABCUL<model_object_name>$ABCUL<dataset_object_name>$ABCUL<prediction_object_name>" \
+                "semseg[<model_object_name>].predict(<dataset_object_name>) -> <prediction_object_name>."
+            return
+        fi
 
         local model_object_name=$(abcli_clarify_object $3 ...)
         [[ "$do_download" == 1 ]] &&
@@ -52,6 +58,39 @@ function roofAI_semseg() {
 
         [[ "$do_upload" == 1 ]] &&
             abcli_upload object $prediction_object_name
+
+        return
+    fi
+
+    if [ "$task" == "train" ]; then
+        if [ $(abcli_option_int "$options" help 0) == 1 ]; then
+            local options="device=cpu|cuda,~download,dryrun,profile=FULL|QUICK|VALIDATION,register,~upload"
+            abcli_show_usage "semseg train$ABCUL[$options]$ABCUL<dataset_object_name>$ABCUL<model_object_name>" \
+                "semseg.train(<dataset_object_name>) -> <model_object_name>."
+            return
+        fi
+
+        local dataset_object_name=$(abcli_clarify_object $3 ..)
+        [[ "$do_download" == 1 ]] &&
+            abcli_download object $dataset_object_name
+
+        local model_object_name=$(abcli_clarify_object $4 .)
+
+        abcli_log "semseg.train($dataset_object_name) -$device-> $model_object_name."
+
+        local model_object_path=$abcli_object_root/$model_object_name
+        mkdir -pv $model_object_path
+
+        abcli_eval dryrun=$do_dryrun \
+            python3 -m roofAI.semseg train \
+            --device $device \
+            --dataset_path $abcli_object_root/$dataset_object_name \
+            --model_path $model_object_path \
+            --profile $(abcli_option "$options" profile VALIDATION) \
+            "${@:5}"
+
+        [[ "$do_upload" == 1 ]] &&
+            abcli_upload object $model_object_name
 
         return
     fi
--- roofAI/__init__.py
+++ roofAI/__init__.py
@@ -1,6 +1,6 @@
 NAME = "roofAI"
 
-VERSION = "3.43.1"
+VERSION = "3.44.1"
 
 DESCRIPTION = "🏠 everything AI about roofs."
 
