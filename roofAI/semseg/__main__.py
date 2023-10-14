903cf316507ff2baee122034d78eba6b6e9d9f75
kamangir <arash@kamangir.net>
2023-10-13 19:23:11 -0700

    untested train changes - kamangir/bolt#746

--- roofAI/semseg/__main__.py
+++ roofAI/semseg/__main__.py
@@ -2,6 +2,7 @@ import argparse
 import os
 from roofAI import NAME, VERSION
 from roofAI.semseg.model import SemSegModel
+from roofAI.semseg.train import SemSegModelTrainer
 from roofAI.semseg import Profile
 from abcli.logging import crash_report
 from abcli import logging
@@ -42,6 +43,12 @@ parser.add_argument(
     default="cpu",
     help="cpu|cuda",
 )
+parser.add_argument(
+    "--dataset_is_camvid",
+    type=int,
+    default=1,
+    help="0|1",
+)
 args = parser.parse_args()
 
 success = True
@@ -52,6 +59,11 @@ except:
     crash_report(f"bad profile: {args.profile}")
     success = False
 
+dataset_path = args.dataset_path
+if args.dataset_is_camvid:
+    dataset_path = os.path.join(dataset_path, "SegNet-Tutorial/CamVid/")
+logger.info(f"dataset_path: {dataset_path}")
+
 if success:
     success = False
     if args.task == "predict":
@@ -61,13 +73,24 @@ if success:
         )
 
         model.predict(
-            dataset_path=os.path.join(args.dataset_path, "SegNet-Tutorial/CamVid/"),
+            dataset_path=dataset_path,
             output_path=args.prediction_path,
             device=args.device,
             in_notebook=False,
         )
 
         success = True
+    elif args.task == "train":
+        trainer = SemSegModelTrainer(
+            dataset_path=dataset_path,
+            model_path=args.model_path,
+            in_notebook=False,
+            profile=profile,
+        )
+
+        model = trainer.train(
+            device=args.device,
+        )
     else:
         logger.error(f"-{NAME}: {args.task}: command not found.")
 
