ec04acf4407b7fed571e4147c5a8d7056cf6eea2
kamangir <arash@kamangir.net>
2023-10-15 21:06:03 -0700

    more tests - kamangir/bolt#746

--- roofAI/__init__.py
+++ roofAI/__init__.py
@@ -1,5 +1,5 @@
 NAME = "roofAI"
 
-VERSION = "3.95.1"
+VERSION = "3.96.1"
 
 DESCRIPTION = "🏠 everything AI about roofs."
--- roofAI/tests/test_RoofAIDataset.py
+++ roofAI/tests/test_RoofAIDataset.py
@@ -1,2 +1,22 @@
+from abcli.modules import objects
+from roofAI.ingest.dataset import RoofAIDataset, Kind
+from abcli.plugins import cache
+import numpy as np
+
+
 def test_RoofAIDataset():
-    ...
+    dataset = RoofAIDataset(objects.object_path(cache.read("roofAI_ingest_CamVid_v1")))
+
+    subset = "test"
+    index = 4
+
+    record_id = dataset.subsets[subset][index]
+    assert record_id
+
+    image = dataset.get_matrix(subset, record_id, Kind.IMAGE, log=True)
+    assert image.shape == (360, 480, 3)
+    assert image.dtype == np.uint8
+
+    mask = dataset.get_matrix(subset, record_id, Kind.MASK, log=True)
+    assert mask.shape == (360, 480)
+    assert mask.dtype == np.uint8
