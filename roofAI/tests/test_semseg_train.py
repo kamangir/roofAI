5a4cf9448a6aa5b47b300cd0a4c1e2f95a2c4c62
kamangir <arash@kamangir.net>
2023-10-15 20:56:19 -0700

    stronger test - kamangir/bolt#746

--- roofAI/tests/test_semseg_train.py
+++ roofAI/tests/test_semseg_train.py
@@ -1,13 +1,26 @@
 from abcli.modules import objects
 from abcli.plugins import cache
-from roofAI.semseg.interface import train
+from roofAI.semseg.interface import predict, train
 
 
 def test_semseg_train():
+    cache.write("roofAI_semseg_model_CamVid_void_py", "void")
+
     success, _ = train(
         dataset_path=objects.object_path(cache.read("roofAI_ingest_CamVid_v1")),
         model_path=objects.object_path(objects.unique_object("test_semseg_train")),
+        register=True,
+        suffix="void_py",
     )
     assert success
 
-    # TODO: test the model
+    assert predict(
+        model_path=objects.object_path(
+            cache.read("roofAI_semseg_model_CamVid_void_py")
+        ),
+        dataset_path=objects.object_path(cache.read("roofAI_ingest_CamVid_v1")),
+        prediction_path=objects.object_path(
+            objects.unique_object("test_semseg_predict")
+        ),
+        device="cpu",
+    )
