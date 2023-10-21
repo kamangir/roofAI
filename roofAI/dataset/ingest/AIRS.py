e262159e16f158df0bf6d6dbee14068286a9554c
kamangir <arash@kamangir.net>
2023-10-21 15:03:03 -0700

    ingest AIRS - kamangir/bolt#420

--- roofAI/dataset/ingest/AIRS.py
+++ roofAI/dataset/ingest/AIRS.py
@@ -1,3 +1,11 @@
+import os
+from tqdm import tqdm
+from abcli import file
+from roofAI.dataset import RoofAIDataset, DatasetKind, MatrixKind
+from roofAI.semseg.model import chip_width, chip_height
+from roofAI import NAME, VERSION
+from abcli import string
+import numpy as np
 from typing import Dict
 from abcli import path
 from roofAI.semseg.model import chip_width, chip_height
@@ -15,6 +23,7 @@ def ingest_AIRS(
     chip_width: int = chip_width,
     chip_overlap: float = 0.5,
     log: bool = False,
+    in_notebook: bool = False,
 ) -> bool:
     logger.info(
         "ingesting AIRS {} -{}-{}x{}-@{:.0f}%-> {}".format(
@@ -29,6 +38,107 @@ def ingest_AIRS(
         )
     )
 
-    logger.info("wip")
+    cache_dataset = RoofAIDataset(cache_path)
+    ingest_dataset = RoofAIDataset(
+        ingest_path,
+        kind=DatasetKind.CAMVID,
+    ).create(log=log)
+
+    for subset in tqdm(counts.keys()):
+        for matrix_kind in list(MatrixKind):
+            chip_count = counts[subset]
+            for record_id in cache_dataset.subsets[subset]:
+                input_matrix = cache_dataset.get_matrix(
+                    subset,
+                    record_id,
+                    matrix_kind,
+                    log=log,
+                )
+
+                chip_count -= slice_matrix(
+                    input_matrix,
+                    matrix_kind,
+                    chip_height,
+                    chip_width,
+                    chip_overlap,
+                    max_chip_count=chip_count,
+                    path=ingest_dataset.subset_path(subset, matrix_kind),
+                    prefix=record_id,
+                    log=log,
+                )
+
+                if chip_count <= 0:
+                    break
+    file.save_yaml(
+        os.path.join(ingest_path, "metadata.yaml"),
+        {
+            "classes": ingest_dataset.classes,
+            "kind": "CamVid",
+            "source": "AIRS",
+            "ingested-by": f"{NAME}-{VERSION}",
+        },
+        log=True,
+    )
+
+    RoofAIDataset(ingest_path).visualize(
+        subset="test",
+        index=0,
+        in_notebook=in_notebook,
+    )
 
     return True
+
+
+def slice_matrix(
+    input_matrix: np.ndarray,
+    kind: MatrixKind,
+    chip_height: int,
+    chip_width: int,
+    chip_overlap: float,
+    max_chip_count: int,
+    path: str,
+    prefix: str,
+    log: bool = False,
+) -> int:
+    if log:
+        logger.info(
+            "slice_matrix[{}]: {} -{}X{}x{}-@{:.0f}%-> {} - {}".format(
+                string.pretty_shape_of_matrix(input_matrix),
+                kind,
+                max_chip_count,
+                chip_height,
+                chip_width,
+                chip_overlap * 100,
+                path,
+                prefix,
+            )
+        )
+
+    count = 0
+    for y in range(
+        0, input_matrix.shape[0] - chip_height, int(chip_overlap * chip_height)
+    ):
+        for x in range(
+            0, input_matrix.shape[1] - chip_width, int(chip_overlap * chip_width)
+        ):
+            count += 1
+
+            chip = input_matrix[y : y + chip_height, x : x + chip_width]
+
+            assert file.save_image(
+                os.path.join(
+                    path,
+                    "{}-{:05d}-{:05d}.png".format(
+                        prefix,
+                        y,
+                        x,
+                    ),
+                ),
+                chip,
+                log=log,
+            )
+
+            if count >= max_chip_count:
+                return count
+
+    return count
---
+++ roofAI/tests/test_ingest.py
@@ -0,0 +1,16 @@
+from roofAI.dataset.ingest.AIRS import ingest_AIRS
+from abcli.modules import objects
+from abcli.plugins import cache
+
+
+def test_ingest_AIRS():
+    assert ingest_AIRS(
+        cache_path=objects.object_path(cache.read("roofAI_ingest_AIRS_cache")),
+        ingest_path=objects.object_path(objects.unique_object("test_ingest_AIRS")),
+        counts={
+            "test": 2,
+            "train": 2,
+            "val": 2,
+        },
+        log=True,
+    )
