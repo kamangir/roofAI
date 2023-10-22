49ef190f28112599a831ef69a7d185dc5e3a6b2b
kamangir <arash@kamangir.net>
2023-10-22 15:37:00 -0700

    += DECENT - kamangir/bolt#420

--- roofAI/__init__.py
+++ roofAI/__init__.py
@@ -1,5 +1,5 @@
 NAME = "roofAI"
 
-VERSION = "3.131.1"
+VERSION = "3.132.1"
 
 DESCRIPTION = "🏠 everything AI about roofs."
--- roofAI/semseg/__init__.py
+++ roofAI/semseg/__init__.py
@@ -3,6 +3,7 @@ from enum import Enum, auto
 
 class Profile(Enum):
     FULL = auto()
+    DECENT = auto()
     QUICK = auto()
     VALIDATION = auto()
 
@@ -10,6 +11,7 @@ class Profile(Enum):
     def data_count(self):
         return {
             Profile.FULL: -1,
+            Profile.DECENT: 40,
             Profile.QUICK: 20,
             Profile.VALIDATION: 1,
         }[self]
@@ -18,6 +20,7 @@ class Profile(Enum):
     def epoch_count(self):
         return {
             Profile.FULL: 40,
+            Profile.DECENT: 10,
             Profile.QUICK: 5,
             Profile.VALIDATION: 1,
         }[self]
