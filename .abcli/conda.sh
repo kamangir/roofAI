71ce2a36e1d7b451b924bde59e9b38b6b55c70df
kamangir <arash@kamangir.net>
2023-11-04 18:49:32 -0700

    conda refactor - kamangir/bolt#420

--- .abcli/conda.sh
+++ .abcli/conda.sh
@@ -4,7 +4,7 @@ function roofAI_conda() {
     local task=$(abcli_unpack_keyword $1 help)
 
     if [ "$task" == "help" ]; then
-        abcli_show_usage "roofAI conda create_env [dryrun,validate]" \
+        abcli_show_usage "roofAI conda create_env [validate]" \
             "create conda environmnt."
         abcli_show_usage "roofAI conda validate" \
             "validate conda environmnt."
@@ -13,22 +13,9 @@ function roofAI_conda() {
 
     if [ "$task" == "create_env" ]; then
         local options=$2
-        local do_dryrun=$(abcli_option_int "$options" dryrun 0)
         local do_validate=$(abcli_option_int "$options" validate 0)
 
-        conda activate base
-        conda remove -y --name roofAI --all
-
-        conda create -y --name roofAI --clone base
-        conda activate roofAI
-
-        pushd $abcli_path_git/awesome-bash-cli >/dev/null
-        pip3 install -e .
-        popd >/dev/null
-
-        abcli_eval \
-            dryrun=$do_dryrun,path=$abcli_path_git/roofAI \
-            pip3 install -e .
+        abcli_conda create_env clone=base,name=roofAI
 
         pip3 install pymysql==0.10.1
         pip3 install -U albumentations[imgaug]
@@ -37,8 +24,7 @@ function roofAI_conda() {
         pip3 install efficientnet_pytorch
         pip3 install segmentation_models_pytorch
 
-        [[ "$do_validate" == 1 ]] &&
-            roofAI_conda validate
+        [[ "$do_validate" == 1 ]] && roofAI_conda validate
 
         return
     fi
--- roofAI/__init__.py
+++ roofAI/__init__.py
@@ -1,5 +1,5 @@
 NAME = "roofAI"
 
-VERSION = "3.179.1"
+VERSION = "3.180.1"
 
 DESCRIPTION = "🏛️  everything AI about roofs."
