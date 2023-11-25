ed7223a8335ee27830c2bbd17b2111169a4f5050
kamangir <arash@kamangir.net>
2023-11-24 21:55:45 -0800

    roofAI-3.206.1 QGIS expressions - kamangir/bolt#420

--- roofAI/QGIS/expressions/vanwatch_label.py
+++ roofAI/QGIS/expressions/vanwatch_label.py
@@ -2,8 +2,7 @@ import math
 from qgis.core import *
 from qgis.gui import *
 
-
-@qgsfunction(args="auto", group="Custom", referenced_columns=[])
+@qgsfunction(args='auto', group='Custom', referenced_columns=[])
 def vanwatch_label(row, webdings, feature, parent):
     """
     Produce label text for a vanwatch mapid.
@@ -11,43 +10,36 @@ def vanwatch_label(row, webdings, feature, parent):
     vanwatch_label(row)
     """
     row = {
-        keyword: value
+        keyword:value
         for keyword, value in row.items()
-        if isinstance(value, int) and value != 0
-    }
+        if isinstance(value,int) and value != 0}
 
-    scale = {"car": 10, "person": 10}
     symbol = {
         "bicycle": "",
         "car": "",
         "person": "",
     }
+    scale = 10
 
     if webdings:
         output = "".join(
             [
-                thing
-                for thing in [
+                thing for thing in [
                     "".join(
-                        math.ceil(row.get(thing, 0) / scale.get(thing, 1))
-                        * [symbol.get(thing, "x")]
-                    )
-                    for thing in symbol
-                ]
-                if thing
-            ]
-        )
+                        math.ceil(row.get(thing,0) / scale) * [symbol.get(thing,"x")]
+                        ) for thing in symbol.keys()
+                ] if thing])
         if not output:
             return output
-
+                
         side = math.ceil(math.sqrt(len(output)))
-        matrix = {0: []}
+        matrix = {0:[]}
         j = 0
-        for ch in output:
-            matrix[j] += [ch]
+        for i in range(len(output)):
+            matrix[j] += [output[i]]
             if len(matrix[j]) >= side:
                 j += 1
-                matrix[j] = []
+                matrix[j] = []        
         return "\n".join(["".join(thing) for thing in matrix.values()])
 
-    return ", ".join(sorted([f"{keyword}:{value}" for keyword, value in row.items()]))
+    return ", ".join(sorted(["{}:{}".format(keyword,value) for keyword,value in row.items()]))
