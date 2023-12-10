2cf470cb44c03d4cf938b61a1e53da2bb76db8f7
kamangir <arash@kamangir.net>
2023-12-09 17:46:52 -0800

    inference - kamangir/bolt#123

--- roofAI/__init__.py
+++ roofAI/__init__.py
@@ -1,5 +1,5 @@
 NAME = "roofAI"
 
-VERSION = "3.246.1"
+VERSION = "3.247.1"
 
 DESCRIPTION = "🏛️  everything AI about roofs."
---
+++ roofAI/inference/classes.py
@@ -0,0 +1,77 @@
+import boto3
+from sagemaker import image_uris
+import sagemaker
+from abcli import logging
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class InferenceClient(object):
+    def __init__(
+        self,
+        verbose: bool = False,
+    ):
+        self.verbose = verbose
+
+        self.region = boto3.Session().region_name
+        self.client = boto3.client("sagemaker", region_name=self.region)
+
+        # Role to give SageMaker permission to access AWS services.
+        # https://github.com/aws-solutions-library-samples/guidance-for-training-an-aws-deepracer-model-using-amazon-sagemaker/issues/47#issuecomment-607238430
+        try:
+            self.sagemaker_role = sagemaker.get_execution_role()
+        except ValueError:
+            logger.info("sagemaker_role: defaulting to local.")
+            iam = boto3.client("iam")
+            self.sagemaker_role = iam.get_role(RoleName="Sagemaker-Access")["Role"][
+                "Arn"
+            ]
+
+        # Get container image (prebuilt example)
+        self.container = image_uris.retrieve("xgboost", self.region, "0.90-1")
+
+        logger.info(f"{self.__class__.__name__} created.")
+
+    def create(
+        self,
+        model_name: str,
+        verify: bool = True,
+    ) -> bool:
+        logger.info(f"creating {model_name}...")
+
+        if self.exists(model_name):
+            logger.info(f"{model_name} already exists, will delete first.")
+            self.delete(model_name)
+
+        # Create the model
+        response = self.client.create_model(
+            ModelName=model_name,
+            ExecutionRoleArn=self.sagemaker_role,
+            Containers=[
+                {
+                    "Image": self.container,
+                    "Mode": "SingleModel",
+                    "ModelDataUrl": f"s3://kamangir/bolt/{model_name}.tar.gz",
+                }
+            ],
+        )
+        if self.verbose:
+            logger.info(f"create({model_name}): {response}")
+
+        return self.exists(model_name) if verify else True
+
+    def delete(self, model_name: str):
+        # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sagemaker/client/delete_model.html#
+        response = self.client.delete_model(ModelName=model_name)
+        if self.verbose:
+            logger.info(f"delete({model_name}): {response}")
+
+    def exists(self, model_name: str) -> bool:
+        # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sagemaker/client/list_models.html
+        response = self.client.list_models(NameContains=model_name)
+
+        if self.verbose:
+            logger.info(f"exists({model_name}): {response}")
+
+        return bool(response["Models"])
