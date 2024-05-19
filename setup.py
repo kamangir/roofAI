fff1f1e90cce36ed10a8276e059e88d4ffac6f93
kamangir <arash@kamangir.net>
2024-05-19 16:02:33 -0700

    += pypi - kamangir/bolt#746

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 from setuptools import setup
+import os
 
 from roofAI import NAME, VERSION, DESCRIPTION
+
+with open(os.path.join(os.path.dirname(__file__), "README.md")) as f:
+    long_description = f.read().replace(
+        "./",
+        "https://github.com/kamangir/roofAI/raw/main/",
+    )
 
 setup(
     name=NAME,
     author="arash@kamangir.net",
     version=VERSION,
     description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=[NAME],
 )
