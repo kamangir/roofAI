1fce3612fd4fe74a5b860e573f3ba18833adc96d
kamangir <arash@kamangir.net>
2024-05-26 14:53:01 -0700

    refactor - kamangir/bolt#746

--- .github/workflows/bashtest.sh
+++ .github/workflows/bashtest.sh
@@ -8,7 +8,7 @@ function bashtest() {
     cd ..
     git clone https://github.com/kamangir/awesome-bash-cli.git
 
-    source $(pwd)/awesome-bash-cli/bash/abcli.sh ~terraform \
+    source $(pwd)/awesome-bash-cli/abcli/.abcli/abcli.sh ~terraform \
         $plugin_name test
 
     return
