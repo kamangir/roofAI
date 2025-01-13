b979edce39babc3d136ea5626125f1f676eed6af
kamangir <arash@kamangir.net>
2025-01-12 20:36:43 -0800

    pipeline wip - kamangir/bolt#746

--- roofAI/.abcli/install.sh
+++ roofAI/.abcli/install.sh
@@ -4,9 +4,10 @@ function abcli_install_roofAI() {
 
     [[ "$abcli_is_sagemaker_system" == true ]] && return 0
 
+    # done in .github/workflows
     [[ "$abcli_is_github_workflow" == true ]] && return 0
 
-    local filename="$HOME/torch/hub/checkpoints/se_resnext50_32x4d-a260b3a4.pth"
+    local filename="$HOME/.cache/torch/hub/checkpoints/se_resnext50_32x4d-a260b3a4.pth"
 
     [[ -f "$filename" ]] && return 0
 
@@ -20,4 +21,4 @@ function abcli_install_roofAI() {
         -o $filename
 }
 
-abcli_install_module roofAI 1.1.1
+abcli_install_module roofAI 1.2.1
